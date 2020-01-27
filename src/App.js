f947a99789edbd8618f4dca9b27da18d048fdeb3
RikuVirtanen <rikvir11@gmail.com>
2020-01-27 14:29:50 +0200

    ei fucked versio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery} from '@apollo/react-hooks';
+import gql from "graphql-tag";
+
+const HSL_KYSELY = gql`
+  {
+  stops(name: "gransinmäk") {
+    name
+    patterns {
+        route {
+            shortName
+        }
+    }
+    stoptimesWithoutPatterns {
+        realtimeArrival
+        headsign
+        serviceDay
+  }
+}
+}
+`;
+
+
+function muunnos(a, b) {
+  var d = new Date();
+  var aika = d.getTime();
+  var myDate = new Date((a + b) * 1000);
+  var saapuu = myDate - aika;
+  return millisToMinutes(saapuu);
+}
+
+function millisToMinutes(millis) {
+  var minutes = Math.floor(millis / 60000);
+  return ' arrives in : ' + minutes + ' min';
+}
+
 
 function App() {
+const {data , loading, error} = useQuery(HSL_KYSELY);
+
+if (loading) return <p>LOADING</p>;
+if (error) return <p>ERROR {error.message}</p>
+
+console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        {data && data.stops && data.stops.map((stops, index) => (
+          <div key = {index} className="taulu">
+            <h1>{stops.name}</h1>
+        {stops.stoptimesWithoutPatterns.map((stoptimesWithoutPatterns, indx) => {
+          return <p key={indx} className="asema">{stoptimesWithoutPatterns.headsign}{muunnos(stoptimesWithoutPatterns.serviceDay, stoptimesWithoutPatterns.realtimeArrival)}</p>
+        })}
+          </div>
+        ))}
+        
+      </div>
+    </React.Fragment>
   );
 }
 
